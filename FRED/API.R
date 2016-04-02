89fa8acdb50739bf0929e3bef16049bca16d4e6b
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2016-04-02 13:50:26 -0500

    Create API.R

---
+++ FRED/API.R
@@ -0,0 +1,16 @@
+## code for API
+
+# API pull code
+
+library(rjson)
+library(httr)
+
+xml_fred <- 'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456'
+
+json_fred <-'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456&file_type=json'
+
+api_key <- '41fea226d2feadb38bba9391f3b58f41'
+file_type <-
+series_id <-
+realtime_start <-  
+realtime_end <-
