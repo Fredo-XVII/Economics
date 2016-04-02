3cfe21bd540749529381dbebaa0ed9720daa7d21
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2016-04-02 14:11:27 -0500

    Update API.R

--- FRED/API.R
+++ FRED/API.R
@@ -1,16 +1,22 @@
-## code for API
-
 # API pull code
 
 library(rjson)
 library(httr)
 
-xml_fred <- 'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456'
+xml_fred <- https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456
 
-json_fred <-'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456&file_type=json'
+json_fred <-https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456&file_type=json
 
-api_key <- ''
-file_type <-
-series_id <-
-realtime_start <-  
-realtime_end <-
+api_url <- https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=
+
+api_key <- 
+file_type <- ''
+series_id <-A191RL1Q225SBEA
+realtime_start <- '' ## 'YYYY-MM-DD'
+realtime_end <- ''  ## 'YYYY-MM-DD'
+
+api_search <- paste0(api_url,api_key,&file_type=json)
+api_search  
+GET(json_fred)
+content(GET(json_fred))
+  
