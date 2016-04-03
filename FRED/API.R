d754debf2df563e47bc16583b68810c85429af2c
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2016-04-02 21:47:13 -0500

    Update API.R

--- FRED/API.R
+++ FRED/API.R
@@ -1,4 +1,5 @@
 # API pull code
+# sources : https://research.stlouisfed.org/docs/api/fred/series_observations.html
 
 library(rjson)
 library(httr)
@@ -8,11 +9,11 @@ xml_fred <- 'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcd
 
 json_fred <-'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456&file_type=json'
 
-api_url <- 'https://api.stlouisfed.org/fred/series?'
+api_url <- 'https://api.stlouisfed.org/fred/series/observations?'
 
 api_key <- '41fea226d2feadb38bba9391f3b58f41'
 file_type <- 'json'
-series_id <- 'GNPCA'
+series_id <- 'A191RL1Q225SBEA'
 realtime_start <- '1929-01-01' ## 'YYYY-MM-DD'
 realtime_end <- '2016-01-01'  ## 'YYYY-MM-DD'
 
@@ -20,8 +21,8 @@ api_search <- paste0(api_url,'series_id=',series_id,'&realtime_start=',realtime_
 api_search  
 GET(api_search)
 data_json <- content(GET(api_search))
-data_json
+data_json$observations
 fromJSON(api_search)
 data_json <- fromJSON(api_search)
-data_json
+data_json[]
   
