734a76d1a1e04ddbe6340f657c9577c0d3527c89
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2017-02-01 17:05:43 -0600

    Update Dev_Extract_LAT_LON.R

--- Gazetteer_Files/Dev_Extract_LAT_LON.R
+++ Gazetteer_Files/Dev_Extract_LAT_LON.R
@@ -1,5 +1,10 @@
+
 gaz_url <- 'http://www2.census.gov/geo/docs/maps-data/data/gazetteer/2016_Gazetteer/2016_Gaz_cbsa_national.zip'
 
 gaz_data <- download.file(url = gaz_url, "data.zip")
 
-data <- read.table("2016_Gaz_cbsa_national.txt", sep = "\t")
+gaz_unzip <- unzip("data.zip")
+
+#data <- read.table("2016_Gaz_cbsa_national.txt", sep = "\t", header = TRUE)
+
+data <- read.table(gaz_unzip, sep = "\t", header = TRUE)
